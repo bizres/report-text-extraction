09819bad7430ca057ba068116537571de310ce63
JohannesHool <hooljohannes@gmail.com>
2022-01-27 15:54:50 +0100

    Update pdf.py
    - Iterate over every element of the pdf to ignore tables, images, headers and footers -> won't be used for text analysis
    - remove whitespace characters such as new lines inside paragraphs/sections
    - Seperate paragraphs with a new line
    - Seperate pages with a double new line

--- pdf.py
+++ pdf.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-from pdfminer.high_level import extract_text
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
 import io
 
 
@@ -13,8 +14,35 @@ def to_text(data):
         file = io.BytesIO(file.read())
 
     try:
-        text = extract_text(file)
+        full_text = ''
+
+        # Iterate over every page of document
+        for page_layout in extract_pages(data):
+
+            page_text = ''
+
+            # Iterate over every container on page
+            for element in page_layout:
+
+                # Only consider containers which are marked as text containers (no headers, footers, tables or images)
+                if isinstance(element, LTTextContainer):
+
+                    section_text = ''
+
+                    # Iterate over every line of text container
+                    for text_line in element:
+                        section_text += text_line.get_text().replace('\n', ' ')
+                    
+                    # Remove all whitespaces (except ' ') such as new lines or tabs
+                    section_text = ' '.join(section_text.split())
+
+                    page_text += section_text # Add section text to page text
+                    page_text += '\n' # Separate sections with a new line
+
+            full_text += page_text # Add page text to full text
+            full_text += '\n' # Separate pages with a double new line
+
     except:
-        text = None
+        full_text = None
         
-    return text
+    return full_text
